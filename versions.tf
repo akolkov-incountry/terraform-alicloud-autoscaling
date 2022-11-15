ccf5542e4e4d107c0a6b6cabeaf24730c336400f
Andrey Kolkov <andrey.kolkov@incountry.com>
2022-11-15 16:36:52 +0400

    add required provider version

--- versions.tf
+++ versions.tf
@@ -1,4 +1,11 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "1.165.0"
+    }
+  }
 }
